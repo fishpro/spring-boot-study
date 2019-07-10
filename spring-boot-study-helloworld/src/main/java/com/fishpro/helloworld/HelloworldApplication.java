272562d73a0477b4793566c657e4f4d7b4da5354
fishpro <fishpro@vip.qq.com>
2019-07-10 23:23:21 +0800

    增加了成功后打印成功信息

--- spring-boot-study-helloworld/src/main/java/com/fishpro/helloworld/HelloworldApplication.java
+++ spring-boot-study-helloworld/src/main/java/com/fishpro/helloworld/HelloworldApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HelloworldApplication.class, args);
+		System.out.println("helloworld application success...");
 	}
 
 }
